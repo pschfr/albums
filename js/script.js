4139fd3f19a50db3cab4f6feb34794797350fea9
Paul Schaefer <yeppaul2@gmail.com>
2016-10-20 12:46:38 -0400

    fixed track not displaying bug

--- js/script.js
+++ js/script.js
@@ -31,25 +31,21 @@ function listens() {
 	var element   = document.getElementById('lastFM');
 	var xmlhttp   = new XMLHttpRequest();
 
-	xmlhttp.open('GET', lastFMurl, true); // begins request to Last.FM
+	xmlhttp.open('GET', lastFMurl, true);
 
 	xmlhttp.onreadystatechange = function() {
-	    if (xmlhttp.readyState == 4) {			              // When Last.FM is ready,
-	        if(xmlhttp.status == 200) {			              // And we have text,
-	            var obj = JSON.parse(xmlhttp.responseText);   // we parse the JSON,
-				var track      = obj.recenttracks.track[0];   // and fetch data from the track
-				var artistName = track.artist['\#text'];
-				var albumName  = track.album['\#text'];
-				var songName   = track.name;
-				var songURL    = track.url;
+	    if (xmlhttp.readyState == 4) {
+	        if(xmlhttp.status == 200) {
+	            var obj = JSON.parse(xmlhttp.responseText);
+				var artistName = obj.recenttracks.track[0].artist['\#text'];
+				var albumName  = obj.recenttracks.track[0].album['\#text'];
+				var songName   = obj.recenttracks.track[0].name;
+				var songURL    = obj.recenttracks.track[0].url;
 
-				element.innerHTML = ''; // removes any existing text
-
-				if (track['\@attr'] && track['\@attr'].nowplaying !== '') // if currently listening
-					element.innerHTML += '<a href="' + songURL + '" title="' + albumName + '">' + artistName + ' &mdash; ' + songName + '</a> ';
+				element.innerHTML = '<a href="' + songURL + '" title="' + albumName + '">' + artistName + ' &mdash; ' + songName + '</a> ';
 	         }
 	    }
 	};
-	xmlhttp.send(null); // Close connection
+	xmlhttp.send(null);
 }
 listens();
